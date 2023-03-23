60caf2d060d656972ed4bf5a1eb58b739f93094b
hiletmis <hayrettin@ennowallet.com>
2023-03-23 15:00:18 +0300

    commit error

--- Sources/AvalancheSwift/Models.swift
+++ Sources/AvalancheSwift/Models.swift
@@ -8,30 +8,14 @@
 import BigInteger
 import Foundation
 
-public class DelegatorInfo {
+// MARK: - DelegatorInfo
+public struct DelegatorInfo {
     public var nodeId: String?
     public let startTime: Int64
     public let endTime: Int64
     public let weight: String
     public let rewardAddress: String
     public var shares: Int32?
-    
-    init(nodeId: String? = nil, startTime: Int64, endTime: Int64, weight: String, rewardAddress: String, shares: Int32? = nil) {
-        self.nodeId = nodeId
-        self.startTime = startTime
-        self.endTime = endTime
-        self.weight = weight
-        self.rewardAddress = rewardAddress
-        self.shares = shares
-    }
-    
-    public func setNode(nodeId: String? = nil) {
-        self.nodeId = nodeId
-    }
-    
-    public func setShares(shares: Int32? = nil) {
-        self.shares = shares
-    }
 }
 
 // MARK: - AddressBatch
