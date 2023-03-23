4a63cd1c3293b7d158e4e422c8136e1d9f94bc99
hiletmis <hayrettin@ennowallet.com>
2023-03-23 14:57:45 +0300

    commit error

--- Sources/AvalancheSwift/Models.swift
+++ Sources/AvalancheSwift/Models.swift
@@ -8,15 +8,6 @@
 import BigInteger
 import Foundation
 
-public struct DelegatorInfo {
-    public var nodeId: String?
-    public let startTime: Int64
-    public let endTime: Int64
-    public let weight: String
-    public let rewardAddress: String
-    public var shares: Int32?
-}
-
 // MARK: - AddressBatch
 struct AddressBatch: Codable {
     let walletAddresses: [String]
@@ -322,5 +313,16 @@ public struct Delegator: Codable {
 public struct RewardOwner: Codable {
     public let locktime, threshold: String
     public let addresses: [String]
+}
+
+// MARK: - DelegatorInfo
+
+public struct DelegatorInfo {
+    public var nodeId: String?
+    public let startTime: Int64
+    public let endTime: Int64
+    public let weight: String
+    public let rewardAddress: String
+    public var shares: Int32?
 }
 
